791c5eb4600bc216b9224929b755051688d34623
Eduardo <edward.steinmann@outlook.com>
2024-07-22 01:32:22 -0400

    upload code

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# brainf_ck
+# Brainf_ck
+Brainf*ck interpreter written in approximately 100 lines of C
---
+++ brainf_ck.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
+
+#define CELLS_SIZE 30000
+
+int main(int argc, char** argv)
+{
+    if (argc == 2)
+    {
+        FILE* file = fopen(argv[1], "r");
+
+        if (file == NULL)
+        {
+            fprintf(stderr, "File %s could not be found or the system does not have enough resources", argv[1]);
+            return EXIT_FAILURE;
+        }
+
+        static uint8_t cells[CELLS_SIZE] = { 0 };
+
+        static uint8_t* data_pointer = cells;
+
+        int character = 0;
+
+        size_t bracket_not_matched = 0;
+
+        while ((character = fgetc(file)) != EOF)
+        {
+            switch (character)
+            {
+                case '>':
+                {
+                    if (data_pointer + 1 > cells + (CELLS_SIZE - 1))
+                    {
+                        fprintf(stderr, "Error: Cannot go past memory tape...");
+                        return EXIT_FAILURE;
+                    }
+                    data_pointer++;
+                    break;
+                }
+                case '<':
+                {
+                    if (data_pointer - 1 < cells)
+                    {
+                        fprintf(stderr, "Error: Cannot go before memory tape...");
+                        return EXIT_FAILURE;
+                    }
+                    data_pointer--;
+                    break;
+                }
+                case '+': (*data_pointer)++; break;
+                case '-': (*data_pointer)--; break;
+                case '.': printf("%c", *data_pointer); break;
+                case ',': *data_pointer = fgetc(stdin); break;
+                case '[':
+                {
+                    if (*data_pointer == 0)
+                    {
+                        bracket_not_matched++;
+                        while (bracket_not_matched)
+                        {
+                            character = fgetc(file);
+                            if (character == EOF)
+                            {
+                                fprintf(stderr, "Error: did not find matching ']'...");
+                                return EXIT_FAILURE;
+                            }
+                            bracket_not_matched += character == '[';
+                            bracket_not_matched -= character == ']';
+                        }
+                    }
+                    break;
+                }
+                case ']':
+                {
+                    if (*data_pointer)
+                    {
+                        bracket_not_matched++;
+                        while (bracket_not_matched)
+                        {
+                            fseek(file, -2, SEEK_CUR);
+                            character = fgetc(file);
+                            if (character == EOF)
+                            {
+                                fprintf(stderr, "Error: did not find matching '['...");
+                                return EXIT_FAILURE;
+                            }
+                            bracket_not_matched -= character == '[';
+                            bracket_not_matched += character == ']';
+                        }
+                    }
+                    break;
+                }
+                default: break;
+            }
+        }
+
+        fclose(file);
+    }
+    else
+    {
+        fprintf(stderr, "Error: Expected source code file name...");
+        return EXIT_FAILURE;
+    }
+
+    return 0;
+}
